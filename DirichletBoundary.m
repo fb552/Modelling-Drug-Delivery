08757b8c814b4abc76f1d4b7d777b2dd7b96d11b
Francesco Berteau <82942517+fb552@users.noreply.github.com>
2023-11-29 19:41:42 +0000

    Update comments

--- DirichletBoundary.m
+++ DirichletBoundary.m
@@ -1,42 +1,27 @@
-function [RHS,GlobalMatrix] = DirichletBoundary(boundary,mesh,GlobalMatrix,RHS)
-%This function enforces Dirichlet boundary conditions by setting matrix
-%rows to zero that correspond to Dirichlet boundary nodes, setting those
-%rows of the RHS vector equal to the boundary condition value and setting
-%the diagonal entry of the BC matrix rows to 1.
+function [RHS,GlobalMatrix] = DirichletBoundary(boundary,RHS,GlobalMatrix)
+%Applies Dirichlet Boundary Conditions to transient diffusion equation
+% Sets the global matrix rows corresponding to boundary nodes to zero and
+% the RHS vector to the boundaries values.
 %
-%Input arguments:
-%Boundary - Data structure containing all the boundary conditions
-%mesh - Finite element mesh
-%GlobalMatrix - Global Matrix without Dirichlet boundary conditions
-%RHS - RHS vector without boundary conditions
+% Input:
+% Boundary : Data structure containing all the boundary conditions
+% RHS - RHS vector
+% GlobalMatrix : Global Matrix
 %
-%Return arguments:
-%RHS - New RHS vector with Dirichlet boundary conditions
-%GlobalMatrix -New Global Matrix with Dirichlet boundary conditions
+% Return:
+% RHS : RHS vector with DBC applied
+% GlobalMatrix : Global Matrix with DBC applied
+%
+% Francesco Berteau (fb552) - November 2023
     
-    %initialise boundary conditions vector with zeros
-    DB = zeros(mesh.ngn,1);
-    %Dirichlet boundary conditions
-    DB{1} = boundary.DirichletL;        %lower BC
-    DB{mesh.ngn} = boundary.DirichletR; %upper BC
-
     %upper Dirichlet BC
     GlobalMatrix(end, :) = 0;
     GlobalMatrix(end) = 1;
-    RHS(end) = DB(mesh.ngn);
+    RHS(end) = boundary.DirichletU;
 
     %lower Dirichlet BC
     GlobalMatrix(1, :) = 0;
     GlobalMatrix(1, 1) = 1;
-    RHS(1) = DB(1);
-
-%     %Loop to apply upper and lower boundary conditions
-%     for i = [1 mesh.ngn]
-%         % upper and lower DBC
-%         if isnumeric(DB{i})
-%             GlobalMatrix(i,:) = 0;
-%             GlobalMatrix(i,i) = 1;
-%             RHS(i) = DB{i};
-%         end
-%     end      
+    RHS(1) = boundary.DirichletL;
+     
 end
