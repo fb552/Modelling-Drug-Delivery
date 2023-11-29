e58a77b416bf467e8d8ff40812440af74a1a2ebb
Francesco Berteau <82942517+fb552@users.noreply.github.com>
2023-11-29 14:59:08 +0000

    GQ inclusion

--- LEMdiffusion.m
+++ LEMdiffusion.m
@@ -1,14 +1,43 @@
-function DiffusionMatrix = LEMdiffusion(D,eN,mesh)
+function [DiffusionMatrix] = LEMdiffusion(eN,mesh,GQ,order)
 % This function calculates the Local 2-by-2 Element Matrix for the linear
 % diffusion operator, for any element, eN, in the finite element mesh.
 % The diffusion coefficient D and the mesh.
     
-    %Jacobian of given element
+    %Jacobian for a given element
     J = mesh.elem(eN).J;
+    %initialise matrix with zeros
+    DiffusionMatrix = zeros(order+1);
+    %initialise values of matrix with zeros
+    Int00 = zeros(order +1);
+    %diffusion coefficient values
+    D = mesh.elem(eN).D;
+     
+    %if manual integration
+    if GQ.Switch == '0'
+        %elements of local element matrix 2J = (x1-x0)
+        Int00 = D/(2*J);
+        %local 2x2 Element Matrix for diffusion
+        DiffusionMatrix = [Int00 -Int00 ; -Int00 Int00];
+    
+    %if Gaussian Quadrature
+    elseif GQ.Switch == '1'
+        %order of solver
+        N = GQ.N; 
+        %get Gaussian points and weights
+        [GQ] = GaussianQuadrature(GQ);
 
-    %elements of local element matrix 2J = (x1-x0)
-    SqMat = D/(2*J);
-
-    %local 2x2 Element Matrix for diffusion
-    DiffusionMatrix = [SqMat, -SqMat; -SqMat, SqMat];
+        for i = 1:N
+            w = GQ.gw(i);          %Gauss weights
+            xipts = GQ.xipts(i);   %Gauss points
+            %basis function at given point
+            [~,dpsidxigrad] = EvalBasis(order,xipts);
+            %local element diffusion matrix
+            for m = 1:order+1
+                for n = 1:order+1
+                    Int00(m,n) = D*dpsidxigrad(m)*dpsidxigrad(n)/J;
+                    DiffusionMatrix(m,n) = DiffusionMatrix(m,n) + w*(Int00(m,n));
+                end
+            end
+        end
+    end
 end
