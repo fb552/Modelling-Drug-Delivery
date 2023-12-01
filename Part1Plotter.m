c3b2aa54b7ade410258c08d6b27c71a4aa872dc6
Francesco Berteau <82942517+fb552@users.noreply.github.com>
2023-12-01 14:01:21 +0000

    new figure titles

--- Part1Plotter.m
+++ Part1Plotter.m
@@ -16,14 +16,14 @@ parameters.f = 0;            %Source term
 Xmin = 0;   %lower spatial boundary
 Xmax = 1;   %upper spatial boundary
 Ne = 10;    %number of elements
-order = 1;  %Selects linear(1) or quadratic(2) basis functions
+order = 1;  %order (linear(1) or quadratic(2)) of basis function
 theta = 1;  %0:Forward Euler, 1:Backward Euler, 0.5:Crank-Nicolson
-Xpoints = Xmin:0.01:Xmax; %vector with all the needed x points
+Xpoints = Xmin:0.01:Xmax; %vector with x points for analytical plotting
 
 % Time related values
 time.tmin = 0;  %start time
 time.tmax = 1;  %end time
-time.ic = 0;    %initial conditiopn time
+time.ic = 0;    %initial condition time
 time.dt = 0.01; %delta t
 time.t = time.tmin:time.dt:time.tmax;   %entire time vector
 time.N = (time.tmax-time.tmin)/time.dt; %number of time steps
@@ -45,25 +45,24 @@ boundary.NeumannU = 'Na';   %Upper Neumann Boundary Condition
 
 %------- Graphs for 1.1 --------------------------------------------------%
 %first figure for computed solution alone
-figure('Name','Part1numerical')
+figure('Name','Part1 numerical')
 for i = 1:length(tpoints)
 
     %Plot c(x) against x
     findplace = time.t == tpoints(i);
-    plot(mesh.nvec,Cnum(:,findplace),'DisplayName',(strcat('t =',num2str(tpoints(i)))),'LineWidth',1.3);
+    plot(mesh.nvec,Cnum(:,findplace),'DisplayName',(strcat('t=',num2str(tpoints(i)))),'LineWidth',1.3);
     hold on
-
-    grid on %use grid lines
-    xlabel('Spatial distance x','FontSize',12)
-    ylabel('Concentration c(x,t)','FontSize',12)
-    legend('Location','NorthWest','FontSize',10)
-
 end
+grid on %use grid lines
+title('Transient FEM numerical solutions','FontSize',14)
+xlabel('Spatial distance x','FontSize',12)
+ylabel('Concentration c(x,t)','FontSize',12)
+legend('Location','NorthWest','FontSize',10)
 % save plot as picture
-saveas(gcf,'Part1numerical','png')
+saveas(gcf,'TransientFEM','png')
 
 %second figure for analytical vs numerical comparison
-figure('Name', 'Part1analytical')
+figure('Name', 'Part1 analytical')
 %Defines line colours using MATLAB RGB triplet
 linecolor = {[0.00 0.45 0.74],[0.85 0.33 0.10], [0.93 0.69 0.13], [0.49 0.18 0.56]};
 Canalytical = zeros(length(Xpoints),length(tpoints));
@@ -75,52 +74,53 @@ for i = 1:length(tpoints)
     end
 
     %plot analytical solution against x
-    plot(Xpoints, Canalytical(:,i),'DisplayName',strcat('Analytical Solution at t = ',num2str(tpoints(i))),'LineStyle','--','LineWidth',1.3,'color', linecolor{i});
+    plot(Xpoints, Canalytical(:,i),'DisplayName',strcat('Analytical Solution @ t=',num2str(tpoints(i))),'LineStyle','--','LineWidth',1.3,'color', linecolor{i});
     hold on
 
     %plot c(x) against x
     findplace = time.t == tpoints(i);
-    plot(mesh.nvec,Cnum(:,findplace),'DisplayName',(strcat('Numerical Solution at t=',num2str(tpoints(i)))),'LineWidth',1.3,'color',linecolor{i});
+    plot(mesh.nvec,Cnum(:,findplace),'DisplayName',(strcat('Numerical Solution @ t=',num2str(tpoints(i)))),'LineWidth',1.3,'color',linecolor{i});
     hold on
-
-    grid on %use grid lines
-    xlabel('Spatial distance x','FontSize',12)
-    ylabel('Concentration c(x,t)','FontSize',12)
-    legend('Location','NorthWest','FontSize',10)
 end
+grid on %use grid lines
+title('Numerical vs Analytical solutions','FontSize',14)
+xlabel('Spatial distance x','FontSize',12)
+ylabel('Concentration c(x,t)','FontSize',12)
+legend('Location','NorthWest','FontSize',10)
 % save plot as picture
-saveas(gcf,'Part1analytical','png')
+saveas(gcf,'TransientFEM-analytical','png')
 
 %------- Graphs for 1.2 --------------------------------------------------%
 %figure for analytical vs numerical comparison @ x = 0.8
-figure('Name','Part1analytical@0.8')
+figure('Name','Part1 analytical @ 0.8')
 
 findplace = mesh.nvec == 0.8;
-plot(time.t,Cnum(findplace,:),'DisplayName','Numerical Solution x=0.8','LineWidth',1.3);
+plot(time.t,Cnum(findplace,:),'DisplayName','Numerical Solution @ x=0.8','LineWidth',1.3);
 hold on
 
 Canalytical = TransientAnalyticSoln(0.8,time.t);
-plot(time.t,Canalytical,'DisplayName','Analytical Solution x=0.8','LineWidth',1.3);
+plot(time.t,Canalytical,'DisplayName','Analytical Solution @ x=0.8','LineWidth',1.3);
 hold on
 
 grid on %use grid lines
+title('Numerical vs Analytical solution at x=0.8','FontSize',14)
 xlabel('Time t (s)','FontSize',12)
 ylabel('Concentration c(0.8,t)','FontSize',12)
 legend('Location','SouthEast','FontSize',10)
 % save plot as picture
-saveas(gcf,'Part1analytical@08','png')
+saveas(gcf,'TransientFEM-analytical@08','png')
 
 %------- Backward Euler vs Crank-Nicolson --------------------------------%
 %
-figure('Name','Crank-Nicolson and Backward at x = 0.8')
+figure('Name','Crank-Nicolson, Backward and Forward @ x = 0.8')
 
 
-for theta = [0.5 1]
+for theta = [0 0.5 1]
     %get numerical solution for given theta
     [Cnum,mesh,GQ,time,GM] = TransientFEM(Xmin,Xmax,Ne,order,theta,time,GQ,boundary,parameters);
     
     findplace = mesh.nvec == 0.8;
-    plot(time.t,Cnum(findplace,:),'-x','DisplayName',strcat('Numerical Solution Theta = ',num2str(theta)),'LineWidth',1.3);
+    plot(time.t,Cnum(findplace,:),'-x','DisplayName',strcat('Numerical Solution θ=',num2str(theta)),'LineWidth',1.3);
     hold on
 end
 
@@ -129,9 +129,11 @@ Canalytical = TransientAnalyticSoln(0.8,time.t);
 plot(time.t,Canalytical,'DisplayName','Analytical Solution','LineWidth',1.3);
 
 grid on %use grid lines
+title('Crank-Nicolson vs Euler methods','FontSize',14)
 xlabel('Time t (s)','FontSize',12)
 ylabel('Concentration c(0.8,t)','FontSize',12)
 legend('Location','SouthEast','FontSize',10)
-xlim([0,0.3])
+ylim([0,0.8])
+xlim([0,0.2])
 % save plot as picture
-saveas(gcf,'Crank-Nicolson_vs_Backward','png')
+saveas(gcf,'Crank-Nicolson_vs_Euler','png')
