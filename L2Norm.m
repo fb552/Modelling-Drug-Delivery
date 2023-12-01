c80b4108c4919f4b463bf59c87abd099b8ee4208
Francesco Berteau <82942517+fb552@users.noreply.github.com>
2023-12-01 14:02:20 +0000

    L2 norm improved

---
+++ L2Norm.m
@@ -0,0 +1,70 @@
+function [L2Norm,h,gradient] = L2Norm(Xmin,Xmax,elements,order,theta,time,GQ,boundary,parameters)
+%Computes the L2 Norm of the transient diffusion reaction equation.
+% The L2 Norm is the error between the numerical and the analytical
+% solution. This is done to test convergence of the TransientFEM calculator
+% over a range of elements.
+%
+% Input:
+%  Xmin : Lower spatial boundary
+%  Xmax : Upper spatial boundary
+%  elements : given elements
+%  order : weather the basis functions is linear or quadratic
+%  theta : Method selection
+%  time : All time related values combined
+%  GQ : Gaussian Quadrature parameters
+%  boundary : Dirichlet and Neumann conditions combined
+%  parameters : Parameters for current material
+% Return:
+%  L2Norm : L2 Norm error
+%  h : Characteristic length
+%  gradient : Output line gradient
+%
+%Francesco Berteau (fb552) - November 2023
+
+    %Defines the order of the quadrature rule
+    N = GQ.N;
+    %preallocations for fast processing
+    h = zeros(1,length(elements));
+    L2Norm = zeros(1,length(elements));
+    E = zeros(1,N);
+
+    for b = 1:length(elements)
+        %Defines number of elements
+        Ne = elements(b);
+        Etot = zeros(1,Ne); %preallocation for fast processing
+        %length for each element
+        h(b) = ((Xmax-Xmin)/Ne);
+        %solve transient FEM
+        [Cnum,mesh,GQ,time,~] = TransientFEM(Xmin,Xmax,Ne,order,theta,time,GQ,boundary,parameters);
+        %Returns the solution column at the selected time
+        cnumerical = Cnum(:,time.t == time.range);
+        for eN = 1:Ne
+            %Jacobian for a given element
+            J = mesh.elem(eN).J;
+            %rows of the local nodes at given eN
+            Cnoderows = cnumerical(mesh.elem(eN).n(1):mesh.elem(eN).n(end),1);
+            for i = 1:N
+                w = GQ.gw(i);          %Gauss weights
+                xipts = GQ.xipts(i);   %Gauss points
+                %basis function at given point
+                [psi,~] = EvalBasis(order,xipts);
+                %interpolation between nodes
+                CNum = psi'*Cnoderows;
+                %Finds x position
+                x = mesh.elem(eN).x*psi;
+                %analytical solution
+                CAnalyt = TransientAnalyticSoln(x,time.range);
+                %error squared
+                E(i) = w*J*(CAnalyt-CNum)^2;
+            end
+            %total error
+            Etot(eN) = sum(E);
+        end
+        %L2 Norm is RMS of total error
+        L2Norm(b) = sqrt(sum(Etot));
+    end
+    %coefficients of polynomial to fit the data
+    y = polyfit(log(h),log(L2Norm),1);
+    %error gradient
+    gradient = y(1);
+end
--- Part1L2Norm.m
+++ Part1L2Norm.m
@@ -1,110 +1,111 @@
-%This script is used to plot all the required graphs to complete the L2
-%Norm Extension of the Part 1 of the Transient MATLAB-Based FEM Modelling.
-%This extension analyses the convergence rate of the finite element method.
+%This script runs the transient FEM solver and plots the L2 norm used to 
+% test the convergence rate of the finite element method function.
+%
+%Francesco Berteau (fb552) - November 2023
 close all
 
+%------- Imput parameters for L2 norm ------------------------------------%
 
-%--------------------Input parameters for L2 Norm-------------------------%
-xmin = 0; %Lower spatial boundary position
-xmax = 1; %Upper spatial boundary position
-Ne = 10; %Number of elements
-GQ.Switch = '1'; %Use of Gaussian Quadrature
-GQ.N = 3; %Order of solver value
-[GQ] = GQscheme(GQ); %Generates Gaussian quadrature
-time.tmin = 0; %Start value of time interval
-time.tmax = 1; %Last value of time interval
-time.IC = 0; %Initial time condition
-time.dt = 0.0001; %Time step
-time.t = time.tmin:time.dt:time.tmax; %Creates the time interval vector
-time.N = (time.tmax-time.tmin)/time.dt; %Number of time steps
-Boundary.DirichletL = 0; %Defines Lower Dirichlet boundary
-Boundary.DirichletU = 1; %Defines Upper Dirichlet boundary
-Boundary.NeumannL = 'Na'; %Defines Lower Neumann boundary
-Boundary.NeumannU = 'Na'; %Defines Upper Neumann boundary
-matparameters.selection = '1'; %Selects material parameters
-matparameters.D = 1; %Diffusion coefficient
-matparameters.lambda = 0; %Reaction coefficient
-matparameters.f = 0; %Source term
-elementsrange = [2 5 10 15 20 25 30 35 40 50 60]; %Selected element size
-timepoints = [0.05 0.1 0.3 1.0]; %Selected time points
+% Parameters of current material
+parameters.selection = '1';  %material for Part 1
+parameters.D = 1;            %Diffusion coefficient
+parameters.lambda = 0;       %Reaction coefficient
+parameters.f = 0;            %Source term
+
+Xmin = 0;   %lower spatial boundary
+Xmax = 1;   %upper spatial boundary
+Ne = 10;    %number of elements
+elements = [2 5 10 15 20 25 30 35 40 45]; %choosen elements
+
+% Time related values
+time.tmin = 0;  %start time
+time.tmax = 1;  %end time
+time.ic = 0;    %initial conditiopn time
+time.dt = 0.0001; %delta t
+time.t = time.tmin:time.dt:time.tmax;   %entire time vector
+time.N = (time.tmax-time.tmin)/time.dt; %number of time steps
+tpoints = [0.05 0.1 0.3 1.0];   %given time points
+
+GQ.switch = '1';    %Gaussian Quadrature yes or no
+GQ.N = 3;           %order of solver
+[GQ] = GQscheme(GQ);%create Gaussian Quadrature
+
+% All boundaries combined
+boundary.DirichletL = 0;    %Lower Dirichlet Boundary Condition
+boundary.DirichletU = 1;    %Upper Dirichlet Boundary Condition
+boundary.NeumannL = 'Na';   %Lower Neumann Boundary Condition
+boundary.NeumannU = 'Na';   %Upper Neumann Boundary Condition
 
 
-%------------------Linear Vs Quadratic Basis Functions -------------------%
-%Loops through selected elements range switching between linear and
-%quadratic basis functions
-for orderbase = [1 2]
-    figure('Name',strcat('L2Norm Testing with basis order = ',num2str(orderbase)))
-    %%Defines line colours using MATLAB RGB triplet
-    linecolor = {[0.00 0.45 0.74],[0.85 0.33 0.10], [0.93 0.69 0.13], [0.49...
-    0.18 0.56]};
-    %Implements Crank-Nicolson and Backward Euler Numerical method
+%------- Linear Vs Quadratic Basis Function ------------------------------%
+%Loops through selected elements range switching between linear and quadratic basis functions
+for order = [1 2]
+    figure('Name',strcat('L2 Norm test, basis order = ',num2str(order)))
+    %predefined line colours [rgb]
+    linecolor = {[0.00 0.45 0.74],[0.85 0.33 0.10], [0.93 0.69 0.13], [0.49 0.18 0.56]};
+    %Crank-Nicolson or Backward Euler methods
     for theta = [0.5 1]
-        %Loops through all the selected time points
-        for i = 1:length(timepoints)
-            %Calculates L2Norm error for the selected time points
-            time.range = timepoints(i);
-            [L2Norm, h, gradient] = L2Normapproximation(xmin,xmax,elementsrange,Boundary,GQ,...
-            orderbase,theta,time,matparameters);
-            %Switch between Crank-Nicolson and Backward Euler method
+
+        for i = 1:length(tpoints)
+            %get L2 Norm error 
+            time.range = tpoints(i);
+            [L2N, h, gradient] = L2Norm(Xmin,Xmax,elements,order,theta,time,GQ,boundary,parameters);
+            %Crank-Nicolson or Backward Euler methods
             switch theta
                 case 0.5
-                    %Plots L2Norm error against characteristic lenght using a Log-log
-                    %scale plot
-                    loglog(h,L2Norm,'DisplayName',strcat('t = ',num2str(time.range)),...
-                    'LineStyle','--','LineWidth',1.3,'color',linecolor{i});
+                    %plot L2 Norm error against characteristic lenght using log-log plot
+                    loglog(h,L2N,'DisplayName',strcat('t=',num2str(time.range)),'LineStyle','--','LineWidth',1.3,'color',linecolor{i});
                 case 1
-                    %Plots L2Norm error against characteristic lenght using a Log-log
-                    %scale plot
-                    loglog(h,L2Norm,'DisplayName',strcat('t =',num2str(time.range)),'LineWidth',1.3,'color',linecolor{i});
+                    %plot L2 Norm error against characteristic lenght using log-log plot
+                    loglog(h,L2N,'DisplayName',strcat('t=',num2str(time.range)),'LineWidth',1.3,'color',linecolor{i});
             end
             hold on
-            grid on %Sets grid lines
-            xlabel('log(h)','FontSize',12); %Creates xlabel
-            ylabel('log(L2Norm)','FontSize',12); %Creates ylabel
-            legend('Location','SouthEast','FontSize',10,'NumColumns',2) %Creates
-            legend
         end
     end
+    grid on %use grid lines
+    title(strcat('L2 Norm test with basis order=',num2str(order)),'FontSize',14)
+    xlabel('log(h)','FontSize',12);
+    ylabel('log(L2 Norm)','FontSize',12);
+    legend('Location','SouthEast','FontSize',10,'NumColumns',2)
+    % save plot as picture
+    saveas(gcf,strcat('L2Norm_order=',num2str(order)),'png')
 end
 
 
-%-----------------Manual integration Vs. Gaussian quadrature -------------------%
-orderbase = 1;
+%------- Manual integration Vs. Gaussian Quadrature ----------------------%
+order = 1;
 time.range = time.tmax;
-figure('Name','Numerical Methods Convergence ')
-%Loops through selected elements range switching manual integration and
-%gaussian quadrature
+figure('Name','Numerical Methods Convergence')
+%manual or gaussian quadrature integration methods
 for j = [0 1]
     GQ.Switch = num2str(j);
-    %Implements Crank-Nicolson and Backward Euler Numerical method
+    %Crank-Nicolson or Backward Euler methods
     for theta = [0.5 1]
-        %Calculates L2Norm error
-        [L2Norm, h, gradient] = L2Normapproximation(xmin,xmax,elementsrange,Boundary,GQ,...
-        orderbase,theta,time,matparameters);
-        %Defines line colours according to the numerical method
-        %implemented
+        %get L2 Norm error
+        [L2N, h, gradient] = L2Norm(Xmin,Xmax,elements,order,theta,time,GQ,boundary,parameters);
+        %two line colours
         if theta == 0.5
             linecolor = [0.00 0.45 0.74];
         elseif theta == 1
             linecolor = [0.85 0.33 0.10];
         end
-        %Switch between manual and gaussian quadrature integration method
+        %manual or gaussian quadrature
         switch j
             case 0
-                %Plots L2Norm error against characteristic lenght using a Log-log
-                %scale plot
-                loglog(h,L2Norm,'LineWidth',1.3,'color',linecolor);
+                %plot L2 Norm error against characteristic lenght using log-log plot
+                loglog(h,L2N,'LineWidth',1.3,'color',linecolor);
             case 1
-                %Plots L2Norm error against characteristic lenght using a Log-log
-                %scale plot
-                loglog(h,L2Norm,'-x','LineWidth',1.3,'color',linecolor);
+                %plot L2 Norm error against characteristic lenght using log-log plot
+                loglog(h,L2N,'-x','LineWidth',1.3,'color',linecolor);
         end
         hold on
-        grid on %Sets grid lines
-        xlabel('log(h)','FontSize',12); %Creates xlabel
-        ylabel('log(L2Norm)','FontSize',12) %Creates ylabel
-        %Creates legend
-        legend('Crank-Nicolson (Manual)','Backward Euler (Manual)',...
-        'Crank-Nicolson (GQ Scheme)','Backward Euler (GQScheme)','Location','SouthEast','FontSize',10)
     end
 end
+grid on %use grid lines
+title('Numerical Methods Convergence','FontSize',14)
+xlabel('log(h)','FontSize',12);
+ylabel('log(L2 Norm)','FontSize',12)
+legend('Crank-Nicolson (Manual)','Backward Euler (Manual)',...
+'Crank-Nicolson (GQ Scheme)','Backward Euler (GQ Scheme)','Location','SouthEast','FontSize',10)
+% save plot as picture
+saveas(gcf,'NumericalMethodsConvergence','png')
