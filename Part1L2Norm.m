4497d42ccb4344b2016b88d55a95a3ad871ea0c0
Francesco Berteau <82942517+fb552@users.noreply.github.com>
2023-12-04 10:04:09 +0000

    Update comments

--- Part1L2Norm.m
+++ Part1L2Norm.m
@@ -42,7 +42,7 @@ boundary.NeumannU = 'Na';   %Upper Neumann Boundary Condition
 for order = [1 2]
     figure('Name',strcat('L2 Norm test, basis order = ',num2str(order)))
     %predefined line colours [rgb]
-    linecolor = {[0.00 0.45 0.74],[0.85 0.33 0.10], [0.93 0.69 0.13], [0.49 0.18 0.56]};
+    linecolor = {[0.00 0.45 0.75],[0.85 0.35 0.10], [0.95 0.70 0.15], [0.50 0.20 0.55]};
     %Crank-Nicolson or Backward Euler methods
     for theta = [0.5 1]
 
