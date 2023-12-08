2eced44894b753bbbbb2dd5209d07df966ed3f48
Francesco Berteau <82942517+fb552@users.noreply.github.com>
2023-12-08 14:06:33 +0000

    effective dose completed

--- Part2Plotter.m
+++ Part2Plotter.m
@@ -26,7 +26,7 @@ parameters.Xb = 0.01;        %Sub-cutaneous x coordinate
 Xmin = 0;   %lower spatial boundary
 Xmax = 0.01;   %upper spatial boundary
 Ne = 40;    %number of elements
-order = 1;  %order (linear(1) or quadratic(2)) of basis function
+order = 2;  %order (linear(1) or quadratic(2)) of basis function
 theta = 0.5;  %0:Forward Euler, 1:Backward Euler, 0.5:Crank-Nicolson
 
 % Time related values
@@ -39,7 +39,7 @@ time.N = (time.tmax-time.tmin)/time.dt; %number of time steps
 tpoints = [0.05 0.1 0.3 1 2 3 5 7 10 20 30];   %given time points
 
 GQ.switch = '1';    %Gaussian Quadrature yes or no
-GQ.npts = 3;        %number of gauss points (2N-1)
+GQ.npts = 4;        %number of gauss points (2N-1)
 [GQ] = GQscheme(GQ);%create Gaussian Quadrature
 
 % All boundaries combined
@@ -62,8 +62,8 @@ for i = 1:length(tpoints)
     
     %Plots temperature distribution through the tissue for a range of
     %time points
-    findplace = time.t == tpoints(i);
-    plot(mesh.nvec,c(:,findplace),'DisplayName',(strcat('t=',num2str(tpoints(i)))),'LineWidth',1.3,'color',colours(i,:));
+    element = time.t == tpoints(i);
+    plot(mesh.nvec,c(:,element),'DisplayName',(strcat('t=',num2str(tpoints(i)))),'LineWidth',1.3,'color',colours(i,:));
     hold on
 end   
 
@@ -83,90 +83,64 @@ saveas(gcf,'DrugConcentration','png')
 
 %% ------- Drug effectiveness --------------------------------------------%
 figure('Name', 'Effectiveness')
-[c,mesh,GQ,time,GM] = TransientFEM(Xmin,Xmax,Ne,order,theta,time,GQ,boundary,parameters);
 
-%time step
-dt = time.dt;
+% concentration for effectiveness
+ceff = 40;
 
-element = round(order*0.005/(mesh.nvec(end)/mesh.ne));
+K = 0;
+while K < 1000
+    [c,mesh,GQ,time,GM] = TransientFEM(Xmin,Xmax,Ne,order,theta,time,GQ,boundary,parameters);
 
-position = find(c(element,:) > 40,1,'first');
-teff = time.t(position);
-K = trapz(c(element,position:end))*dt;
+    element = round(order*0.005/(mesh.nvec(end)/mesh.ne));
+    position = find(c(element,:) > ceff,1,'first');
+    teff = time.t(position);
 
-plot(time.t,c(element,:));
+    K = trapz(c(element,position:end))*time.dt;
+
+    boundary.DirichletL = boundary.DirichletL + 1;
+end
+
+plot(time.t,c(element,:),'DisplayName','Concentration @ x=0.005','LineWidth',1.3);
 hold on
-plot([1 1]*time.t(position),[0 c(element,position)], '--r')
+plot(teff,c(element,position),'ro', 'MarkerSize', 10,'DisplayName','Minimum effective dose')
+% Add label with coordinates
+label = sprintf('[%.2f, %.1f]', teff, c(element,position));
+text(teff + 1, c(element,position) - 1, label, 'FontSize', 10);
 
+grid on %use grid lines
+title('Drug effectiveness at Dermis end','FontSize',14)
+xlabel('Time t (s)','FontSize',12);
+ylabel('Concentration c(0.005,t)','FontSize',12);
+legend('Location','SouthEast','FontSize',10)
+% save plot as picture
+saveas(gcf,'DrugEffect','png')
 
+fprintf("Minimum initial dose: %d \n",boundary.DirichletL);
 
-fprintf(strcat(" Effectiveness: ",num2str(DrugEffect)),'\n');
-% 
+%% ------- Effective drug concentration ----------------------------------%
+figure('Name', 'Concentration')
 
+%Loop through all the selected time points
+for i = 1:length(tpoints)
+    
+    %Plots temperature distribution through the tissue for a range of
+    %time points
+    element = time.t == tpoints(i);
+    plot(mesh.nvec,c(:,element),'DisplayName',(strcat('t=',num2str(tpoints(i)))),'LineWidth',1.3,'color',colours(i,:));
+    hold on
+end   
 
+%show where the epidermis and dermis end
+xline(parameters.Xe,'-.k','Epidermis','FontSize',12,'LineWidth',0.75,'HandleVisibility','off');
+xline(parameters.Xd,'-.k','Dermis','FontSize',12,'LineWidth',0.75,'HandleVisibility','off');
+%show the minimum concentration for effectiveness
+yline(ceff,'--r','Effectiveness','FontSize',12,'LineWidth',1,'HandleVisibility','off')
 
-
-
-
-
-
-
-
-
-
-
-% %----------------------Minimum temperature reduction ---------------------%
-% 
-% G = [0 0.0375]; %Defines blood flow vector
-% 
-% %Implements linear and quadratic basis functions
-% for orderbase = [1 2]
-%     
-%     %Implements Crank-Nicolson and Backward Euler numerical method
-%     for theta = [0.5 1]
-%         
-%         %Implements blood flow values
-%         for j = 1:length(G)
-%             
-%             matparameters.G = G(j); %Defines blood flow value
-%             
-%             %Calculates numerical solution for the transient problem
-%             [mesh,Cnum,GQ,time,GlobalMatrix] = FiniteElemSolver(Boundary,xmin,xmax,Ne,GQ,orderbase,theta,time,matparameters);
-%             
-%             %Implements dermis and epidermis boundaries
-%             for xtissue = [0.005 0.0016667]
-%                 
-%                 %Calculates temperature reduction
-%                 [TempReduction] = TempReductionCalc(Boundary,xmin,xmax,Ne,GQ,orderbase,theta,time,matparameters,xtissue,Tburn);
-%                 %Write formatted blood flow and temperature reduction data to text file
-%                 fprintf(strcat('\n'," G = ", num2str(G(j))," Temperature reduction required: ",num2str(TempReduction),"(K)"))
-%             end
-%         end
-%     end
-% end
-% 
-% %-------------------------Convergence Analysis----------------------------%
-% 
-% G = [0 0.0375]; %Defines blood flow vector
-% 
-% %Loop to calculate temperature values for each blood flow case
-% for j = 1:length(G)
-%     
-%     matparameters.G = G(j); %Defines blood flow value
-%     
-%     %Calculates numerical solution for the transient problem
-%     [mesh,Cnum,GQ,time,GlobalMatrix] = FiniteElemSolver(Boundary,xmin,xmax,Ne,GQ,orderbase,theta,time,matparameters);
-%     
-%     figure(5)
-%     
-%     %Plots temperature against time at the dermis layer
-%     findplace = mesh.nvec == 0.005;
-%     plot(time.t,Cnum(findplace,:),'DisplayName',(strcat('G = ',num2str(G(j)))),...
-%     'LineWidth',1.3,'color',linecolor{j});
-%     hold on
-%     
-%     grid on %Sets grid lines
-%     xlabel('Time t (s)','FontSize',12); %Creates xlabel
-%     ylabel('Temperature T(0.005,t)','FontSize',12); %Creates ylabel
-%     legend('Location','SouthEast','FontSize',10,'NumColumns',1) %Creates legend
-% end
+grid on %use grid lines
+title('Effective drug concentration through the skin','FontSize',14)
+xlabel('Skin Depth (m)','FontSize',12);
+ylabel('Concentration  c(x,t)','FontSize',12);
+legend('Location','NorthEast','FontSize',10,'NumColumns',2)
+ylim([0,75])
+% save plot as picture
+saveas(gcf,'EffectiveDrugConcentration','png')
